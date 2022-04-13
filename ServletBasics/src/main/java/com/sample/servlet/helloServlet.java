eb8b21688f113a5cba5d7cb2683c0667a167979f
Sanal Mishra <sanamishra@BLR.ALLEGISINDIA.COM>
2022-04-13 13:14:03 +0530

    Servlet Example

---
+++ ServletBasics/src/main/java/com/sample/servlet/helloServlet.java
@@ -0,0 +1,28 @@
+package com.sample.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.catalina.connector.Response;
+
+public class helloServlet extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h3>Hello World from Servlets !!</h3>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+}
